13257564cc30f8848fdc8055761ebd9835bcaf4b
Alexander Logvinov <avl@logvinov.com>
2012-01-29 14:56:39 +0800

    Add a space

--- src/IEProxy.js
+++ src/IEProxy.js
@@ -5,7 +5,7 @@
  */
 
 delete navigator.appName;
-navigator.appName="Microsoft Internet Explorer";
+navigator.appName = "Microsoft Internet Explorer";
 
 delete navigator.appVersion;
 navigator.appVersion = "4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0)";
